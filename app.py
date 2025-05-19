5243ccd22722762f23f28cfb517c55ac829e3ada
zimHTML <zimjitter@gmail.com>
2025-05-19 22:26:31 +0200

    commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, abort
+import os
 
 app = Flask(__name__)
 
@@ -32,5 +33,9 @@ def episode(episode_id):
     else:
         abort(404)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
